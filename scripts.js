75278396f97e9ad120c8d67a126c6f7150ba33be
chandra20500 <jhabrajeshc@gmail.com>
2020-03-09 23:54:37 +0530

    first commit
    lykaa website

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// const selectElement = (s) => document.querySelector(s);
+// selectElement(s,'.open').addEventListener('click',() => {
+//     selectElement(s,'.nav-list').classList.add('active');
+// });
+
+// selectElement(s,'.close').addEventListener('click',() => {
+//     selectElement(s,'.nav-list').classList.remove(tokens,'active');
+// });
+function addClass() { 
+    var v = document.getElementById("navbar"); 
+    v.className += " active"; 
+} 
+console.log("asd");
